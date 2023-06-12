060dd23f7badbbf47fff0eeaeea8055d0d92c876
nidhihegde3108 <nidhihegde3108@gmail.com>
2023-06-12 17:46:17 +0530

    add

---
+++ Professors performance/src/module_1/class_1.java
@@ -0,0 +1,71 @@
+package module_1;
+
+public class class_1 {
+
+	    private String name;
+	    private int yearsOfExperience;
+	    private int classesCount;
+	    private int salary;
+	    private int age;
+	    private int leavesTaken;
+
+	    public class_1(String name, int yearsOfExperience, int classesCount, int salary, int age, int leavesTaken) {
+	        this.name = name;
+	        this.yearsOfExperience = yearsOfExperience;
+	        this.classesCount = classesCount;
+	        this.salary = salary;
+	        this.age = age;
+	        this.leavesTaken = leavesTaken;
+	    }
+
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public int getYearsOfExperience() {
+	        return yearsOfExperience;
+	    }
+
+	    public void setYearsOfExperience(int yearsOfExperience) {
+	        this.yearsOfExperience = yearsOfExperience;
+	    }
+
+	    public int getClassesCount() {
+	        return classesCount;
+	    }
+
+	    public void setClassesCount(int classesCount) {
+	        this.classesCount = classesCount;
+	    }
+
+	    public double getSalary() {
+	        return salary;
+	    }
+
+	    public void setSalary(int salary) {
+	        this.salary = salary;
+	    }
+
+	    public int getAge() {
+	        return age;
+	    }
+	    
+	    public void setAge(int age) {
+	        this.age = age;
+	    }
+
+	    public int getLeavesTaken() {
+	        return leavesTaken;
+	    }
+
+	    public void setLeavesTaken(int leavesTaken) {
+	        this.leavesTaken = leavesTaken;
+	    }
+
+
+}
